4621b6d0317b6bb1c4fd05f34738f64130925113
undernotic <undernotic@gmail.com>
2016-01-19 20:07:53 +0100

    changed conf files loc to be compliant with asp.net5

---
+++ src/KeyJitsu/webpack.config.js
@@ -0,0 +1,17 @@
+﻿var path = require("path");
+
+var config = {
+    entry: "./App/js/app.js",
+    output: {
+        path: path.resolve("wwwwroot/"),
+        publicPath: "/scripts/",
+        filename: "app.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style!css" }
+        ]
+    }
+};
+
+module.exports = config;
