0c7b03cc6d974132c59f4f3b74a1d025012cc46d
undernotic <undernotic@gmail.com>
2016-02-10 22:52:08 +0100

    styling changes

--- KeyJitsu.Client/src/app/dashboard/dashboard.component.ts
+++ KeyJitsu.Client/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import {Component} from 'angular2/core'
+import {Component, OnInit} from 'angular2/core'
 import {Router} from 'angular2/router'
 import {Dragula} from '../shared/directives/dragula.directive'
 import {DragulaService} from '../shared/providers/dragula.provider'
@@ -6,11 +6,38 @@ import {DragulaService} from '../shared/providers/dragula.provider'
 @Component({
     selector: 'dashboard',
     templateUrl: './app/dashboard/dashboard.component.html',
+    styleUrls: ['./app/dashboard/dashboard.component.css'],
     directives: [Dragula],
     viewProviders: [DragulaService]
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+
+    categories: Object[] = []
+
     constructor(
         private _router: Router
     ) { }
+
+    ngOnInit() {
+        this.categories = [{
+            name: "Editing",
+            hotkeys: [
+                {
+                    name: "New file",
+                    hotkey: "Ctrl + N"
+                },
+                {
+                    name: "Cut",
+                    hotkey: "Ctrl + X"
+                }]
+        },
+            {
+                name: "Navigation"
+            },
+            {
+                name: "File Management"
+            }
+        ];
+    }
+
 } 
