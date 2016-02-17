92458b48c47f9c3841983389cc8aea1f863ef615
undernotic <undernotic@gmail.com>
2016-02-18 00:22:52 +0100

    added asp.net 4.6 seed project

---
+++ KeyJitsu.Server/KeyJitsu.Server/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(KeyJitsu.Server.Startup))]
+
+namespace KeyJitsu.Server
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            app.Run(context =>
+            {
+                context.Response.ContentType = "text/plain";
+                return context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
