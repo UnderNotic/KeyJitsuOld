e223d51f24c088d29044ee52217b625b01c0e341
undernotic <undernotic@gmail.com>
2016-02-24 18:25:01 +0100

    Shortcut model

---
+++ KeyJitsu.Server/KeyJitsu.Server/Models/EditorShortcutSheet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace KeyJitsu.Server.Models
+{
+    public class EditorShortcutSheet
+    {
+        public string Editor { get; }
+        public ReadOnlyDictionary<string, ReadOnlyDictionary<string, string>> Categories { get; }
+
+        public EditorShortcutSheet(string editor, ReadOnlyDictionary<string, ReadOnlyDictionary<string, string>> categories)
+        {
+            Editor = editor;
+            Categories = categories;
+        }
+    }
+}
---
+++ KeyJitsu.Server/KeyJitsu.Server/Providers/ShortcutDataProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using KeyJitsu.Server.Models;
+
+public class ShortcutDataProvider
+{
+    public EditorShortcutSheet VisualStudioSheet { get; }
+    public EditorShortcutSheet VsCodeSheet { get; }
+    public EditorShortcutSheet IntellijSheet { get; }
+
+    public ShortcutDataProvider()
+    {
+        VisualStudioSheet = new EditorShortcutSheet("VisualStudio",
+            new ReadOnlyDictionary<string, ReadOnlyDictionary<string, string>>(new Dictionary<string, ReadOnlyDictionary<string, string>>
+            {
+                {"Navigation", new ReadOnlyDictionary<string, string>(new Dictionary<string, string>
+                {
+                    {"Go to somewhere", "Ctrl + X" },
+                    {"Go to somewhere2", "Ctrl + R" }
+                })},
+                {"Refactoring", new ReadOnlyDictionary<string, string>(new Dictionary<string, string>
+                {
+                    {"Refactor1", "Ctrl + Z" },
+                    {"Refactor2", "Ctrl + A" }
+                })}
+            }));
+    }
+}
